fe91558971237ff9200357b369136d740bc73332
Andrew Tait <andrew.tait@sage.com>
2018-02-12 15:26:13 +0000

    added iterm profile

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 function doIt() {
   echo "-- Synchronizing files/folders..."
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
-  --exclude "README.md" -av --no-perms . ~
+  --exclude "iterm-profile.json" --exclude "README.md" -av --no-perms . ~
   echo "-- Setting up vim plugins..."
   rm -rf ~/.vim/bundle/Vundle.vim
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
